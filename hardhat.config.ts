8c836c83916f63584a43aac2f4728d7227e33f0c
allarious <alirezaarjmandshakoori@gmail.com>
2022-07-20 14:10:13 -0600

    updated forking and mining configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { nodeAPIKey } from "./config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      mining: {
+        mempool: {
+          order: "fifo"
+        }
+      },
+      forking: {
+        url: nodeAPIKey,
+      }
+    }
+  }
 };
 
 export default config;
