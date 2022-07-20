e17d1825d924ad71cacd59303ad06f86928ad7b4
allarious <alirezaarjmandshakoori@gmail.com>
2022-07-20 14:19:22 -0600

    added balance task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import { nodeAPIKey } from "./config";
 import "@nomicfoundation/hardhat-toolbox";
 
@@ -17,5 +17,15 @@ const config: HardhatUserConfig = {
     }
   }
 };
+
+// This is just to make sure if everything is working correctly when using witht he cli, can be removed later if it is not needed. 
+// Overall a handy task to have.
+task("balance", "Prints the account balance")
+  .addParam("account", "Address of the account")
+  .setAction(async (taskArgs, hre) => {
+    const address = taskArgs.account;
+    const balance = await hre.ethers.provider.getBalance(address);
+    console.log(hre.ethers.utils.formatEther(balance), "ETH");
+  })
 
 export default config;
