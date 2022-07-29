2ed1eb5719d4147519844d66f9323f321094ca6f
allarious <arjmands@ualberta.ca>
2022-07-28 19:46:04 -0600

    added serialization for types 0 and 1

--- scripts/serialize.ts
+++ scripts/serialize.ts
@@ -1,17 +1,27 @@
 
-import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx'
+import { FeeMarketEIP1559Transaction, AccessListEIP2930Transaction, Transaction } from '@ethereumjs/tx'
 
 
 //TODO can add a transaction class later on
 //TODO add other types of transactions
 export const serialize = (txObject: any) => {
 
-    
+    let tx;
+    const wrappedTx = etherJsTransactionWrapper(txObject)
 
-    const tx = FeeMarketEIP1559Transaction.fromTxData(
-        etherJsTransactionWrapper(
-            txObject
-    ));
+    switch(txObject.type){
+        case "0x0":
+            tx = Transaction.fromTxData(wrappedTx);
+            break
+        case "0x1":
+            tx = AccessListEIP2930Transaction.fromTxData(wrappedTx);
+            break
+        case "0x2":
+            tx = FeeMarketEIP1559Transaction.fromTxData(wrappedTx);
+            break
+        default:
+            throw Error("Invalid Transaction Type!");
+    }
 
     const rawTransaction = "0x" + tx.serialize().toString('hex');
     
