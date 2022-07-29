1204bca22cb704fba616ebc0c29e015fa3c711c6
allarious <arjmands@ualberta.ca>
2022-07-28 19:50:15 -0600

    get raw transactions from a block by number

---
+++ scripts/getBlockRawTransactions.ts
@@ -0,0 +1,19 @@
+import { getBlockTransactions } from "./getBlockTransactions";
+import { getRawTransactionByHash } from "./getRawTransactionByHash";
+
+export const getBlockRawTransactions = async (blockNumber: number) => {
+    const txHashes = await getBlockTransactions(blockNumber);
+
+    if(txHashes === undefined){
+        return undefined;
+    }
+
+    let txRaws = [];
+    let counter = 0
+
+    for(let txHash of txHashes){
+        txRaws.push(await getRawTransactionByHash(txHash))
+    }
+
+    return txRaws
+}
