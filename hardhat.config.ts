44b37a6c8e69dccefc3fe17020c157f134213c28
allarious <arjmands@ualberta.ca>
2022-07-29 18:36:22 -0600

    added more time, since tests need more than 40 secs to run, expecially when mining a new block

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  mocha: {
+    timeout: 100000000
+  },
   networks: {
     hardhat: {
       chainId: 1,
