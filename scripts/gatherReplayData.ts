96cc7887895153bc1be0c9e839b928ce1bac6063
allarious <arjmands@ualberta.ca>
2022-10-24 16:30:50 -0600

    added storage to persist data across runs

--- scripts/gatherReplayData.ts
+++ scripts/gatherReplayData.ts
@@ -2,6 +2,9 @@ import hre from "hardhat";
 import { replayFailedTx } from "./replayFailedTx";
 import storeMapInFile from './utils/storeMapInFile';
 
+import nconf, { file } from "nconf";
+nconf.use('file', { file: './runVariables.json' });
+nconf.load();
 
 export async function gatherReplayDataLive(verbose = true){
     let currentBlock, previousBlock;
@@ -21,14 +24,17 @@ export async function gatherReplayDataLive(verbose = true){
     }
 }
 
-export async function gatherReplayInterval(interval: number[], fileName: string, verbose = false){
+export async function gatherReplayInterval(fileName: string, verbose = false){
     
-    let [firstBlock, lastBlock] = interval;
+    let [firstBlock, lastBlock] = getBlockNumbers(fileName);
     if(lastBlock < firstBlock) throw Error(`invalid interval start ${firstBlock}, end ${lastBlock}`);
     let currrentBlock = await hre.ethers.provider.getBlockNumber();
     if(currrentBlock < lastBlock) throw Error("The last block of interval is not mined yet.")
 
     for(let blockNumber = firstBlock; blockNumber <= lastBlock; blockNumber++){
+
+        updateBlockNumber(fileName + "_start", blockNumber);
+
         if(verbose) console.log(`Extracting block ${blockNumber}.`);
         await replayAndStoreBlockData(blockNumber, fileName);
         if(verbose) console.log(`Finished extracting block number ${blockNumber}`);
@@ -44,7 +50,22 @@ async function replayAndStoreBlockData(blockNumber: number, fileName = "replayDa
     await storeMapInFile(failedTxMap, fileName + ".js", `"blockNumber": ${blockNumber},\n"time": ${finishTime - startingTime},`);
 }
 
-// gatherReplayInterval([14500294, 14850000], "replayData");
+function updateBlockNumber(key: string, value: number, verbose = false){
+    nconf.set(key, value);
+    nconf.save(function (err: any) {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        if(verbose) console.log('Configuration saved successfully.');
+      });
+}
+
+function getBlockNumbers(fileName: string): number[]{
+    return [nconf.get(fileName + "_start"), nconf.get(fileName + "_end")];
+}
+
+gatherReplayInterval("replayData");
 // gatherReplayInterval([14850001, 15200000], "replayData1");
-gatherReplayInterval([15272219, 15600000], "replayData2");
+// gatherReplayInterval([15272219, 15600000], "replayData2");
 // 15200078
