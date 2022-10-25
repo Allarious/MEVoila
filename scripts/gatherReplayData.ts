72283c0695f8baa53692fba2f479df26767e82c5
allarious <arjmands@ualberta.ca>
2022-10-24 20:13:45 -0600

    fixed gather replay data

--- scripts/gatherReplayData.ts
+++ scripts/gatherReplayData.ts
@@ -5,7 +5,7 @@ import storeMapInFile from './utils/storeMapInFile';
 import nconf, { file } from "nconf";
 
 
-export async function gatherReplayDataLive(verbose = true){
+export async function gatherReplayDataLive(verbose = false){
     let currentBlock, previousBlock;
     while(true){
         currentBlock = await hre.ethers.provider.getBlockNumber();
@@ -28,14 +28,14 @@ export async function gatherReplayInterval(fileName: string, verbose = false){
     nconf.use('file', { file: `./scripts/threads/${fileName}.json` });
     nconf.load();
 
-    let [firstBlock, lastBlock] = getBlockNumbers(fileName);
+    let [firstBlock, lastBlock] = getBlockNumbers();
     if(lastBlock < firstBlock) throw Error(`invalid interval start ${firstBlock}, end ${lastBlock}`);
     let currrentBlock = await hre.ethers.provider.getBlockNumber();
     if(currrentBlock < lastBlock) throw Error("The last block of interval is not mined yet.")
 
     for(let blockNumber = firstBlock; blockNumber <= lastBlock; blockNumber++){
 
-        updateBlockNumber(fileName + "_start", blockNumber);
+        updateBlockNumber("replayData_start", blockNumber);
 
         if(verbose) console.log(`Extracting block ${blockNumber}.`);
         await replayAndStoreBlockData(blockNumber, fileName);
@@ -63,11 +63,11 @@ function updateBlockNumber(key: string, value: number, verbose = false){
       });
 }
 
-function getBlockNumbers(fileName: string): number[]{
-    return [nconf.get(fileName + "_start"), nconf.get(fileName + "_end")];
+function getBlockNumbers(): number[]{
+    return [nconf.get("replayData_start"), nconf.get("replayData_end")];
 }
 
-gatherReplayInterval("replayData0");
+// gatherReplayInterval("replayData0");
 // gatherReplayInterval([14850001, 15200000], "replayData1");
 // gatherReplayInterval([15272219, 15600000], "replayData2");
 // 15200078
