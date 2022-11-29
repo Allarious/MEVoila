d8717110661b908ffc074e49a4d16970b6841918
allarious <arjmands@ualberta.ca>
2022-11-28 17:23:57 -0700

    completed data analysis script

--- scripts/dataAnalysis/blockCensorshipAnalysis.ts
+++ scripts/dataAnalysis/blockCensorshipAnalysis.ts
@@ -1,29 +1,111 @@
-
 import {ethers} from "hardhat";
-import storeObjectInFile from "../utils/storeObjectInFile";
+import { fetchReceiptsFromBlock } from "../utils/fetchReceipts";
+import { getFlashbotsBefore } from "../utils/getFlashbotsBefore";
+import data from "../../replayData0";
+import nconf from "nconf";
 
 /**
  * @param numberOfBlocks Number of blocks that is used in the data imported that means the number of blocks replayed, is this really needed?
  */
-async function blockCensorship(numberOfBlocks: number){
-    for(let blockNumber = 14531522; blockNumber < 15500000; blockNumber++){
-        // console.log(blockNumber);
-        let blockData = await ethers.provider.send("eth_getBlockByNumber", ["0x" + blockNumber.toString(16), false]);
-        let {
-            gasLimit,
-            gasUsed
-        } = blockData;
+async function blockCensorship(){
+    nconf.use('file', { file: `./scripts/dataAnalysis/runData.json` });
+    nconf.load();
 
-        let storeObj = {
-            blockNumber,
-            gasLimit: parseInt(gasLimit, 16),
-            gasUsed: parseInt(gasUsed, 16)
+    var flashbotsCounter = 0;
+    var flashbotsBlocks = [];
+    let start = getStartingBlock();
+    for(let i = start; i < data.length; i++){
+        let block = data[i];
+        let obj: any = {};
+        let blockNumber = block["blockNumber"];
+        // console.log(blockNumber);
+        let isFlashBots = false;
+        let flashbotsBlock = null;
+        // console.log(txs);
+        //fetching flashbots if needed
+        if(flashbotsCounter >= flashbotsBlocks.length){
+            flashbotsCounter = 0;
+            flashbotsBlocks = await getFlashbotsBefore(blockNumber + 100);
+            if(flashbotsBlocks.length === 0){
+                throw new Error("Flashbot server failed, reverting");
+            }
+            // console.log(flashbotsBlocks);
+            flashbotsBlocks = flashbotsBlocks.filter((a: any) => a.block_number >= blockNumber);
         }
-        await storeObjectInFile(storeObj, "blockData.js");
+        //fetching blockData
+        let blockData = await ethers.provider.send("eth_getBlockByNumber", ["0x" + blockNumber.toString(16), true]);
+
+        //fetching receipts
+        let blockReceipts: any[] = await fetchReceiptsFromBlock(blockNumber);
+
+        if(flashbotsBlocks[flashbotsCounter]["block_number"] === blockNumber){
+            flashbotsBlock = flashbotsBlocks[flashbotsCounter++];
+            isFlashBots = true;
+        }
+
+        // console.log(blockData);
+        // console.log(blockReceipts);
+        // console.log(flashbotsBlocks);
+
+        obj.hash = blockData.hash;
+        obj.blockNumber = parseInt(blockData.number, 16);
+        obj.isFlashBotsBlock = isFlashBots;
+        obj.gasLimit = parseInt(blockData.gasLimit, 16);
+        obj.gasUsed = parseInt(blockData.gasUsed, 16);
+        obj.timestamp = parseInt(blockData.timestamp, 16);
+        let flashbotsTxs: any = {};
+        if(isFlashBots){
+            for(let [idx, tx] of flashbotsBlock.transactions.entries()){
+                flashbotsTxs["" + tx.transaction_hash] = idx;
+            }
+        }
+        obj.flashBotsTransactions = flashbotsTxs;
+        obj.transactions = [];
+        for(let tx of block.data){
+            let txObject: any = {};
+            let hash = Object.keys(tx)[0];
+            let runAtBlock = Object.values(tx)[0];
+            let txData = blockData.transactions.find((tx: any) => tx.hash === hash);
+            // console.log(txData);
+            let txReceiptData = blockReceipts.find((tx: any) => tx.transactionHash === hash);
+            // console.log(txReceiptData);
+
+            txObject.hash = hash;
+            txObject.runAtBlock = runAtBlock;
+            txObject.type = parseInt(txData.type, 16);
+            txObject.status = parseInt(txReceiptData.status, 16);
+            txObject.gasUsed = parseInt(txReceiptData.gasUsed, 16);
+            txObject.gasLimit = parseInt(txData.gas, 16);
+            txObject.index = parseInt(txData.transactionIndex, 16);
+            txObject.gasPrice = parseInt(txData.gasPrice, 16);
+            txObject.maxFeePerGas = parseInt(txData.maxFeePerGas, 16);
+            txObject.maxPriorityFeePerGas = parseInt(txData.maxPriorityFeePerGas, 16);
+            txObject.effectiveGasPrice = parseInt(txReceiptData.effectiveGasPrice, 16);
+            txObject.cumulativeGasUsed = parseInt(txReceiptData.cumulativeGasUsed, 16);
+
+            obj.transactions.push(txObject);
+        }
+
+        process.stdout.write(JSON.stringify(obj) + ',\n');
+
+        updateStartingBlock("start", i);
     }
 }
+function getStartingBlock(): number{
+    return nconf.get("start");
+}
+function updateStartingBlock(key: string, value: number, verbose = false){
+    nconf.set(key, value);
+    nconf.save(function (err: any) {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        if(verbose) console.log('Configuration saved successfully.');
+      });
+}
 
-blockCensorship(4);
+blockCensorship();
 
 //
 //    "blockNumber": 14499990,
