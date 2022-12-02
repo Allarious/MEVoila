4697f88361234f7ec943a0a033c7005205fe5e0d
allarious <arjmands@ualberta.ca>
2022-12-02 07:53:43 -0700

    wrote a script to merge json files together

---
+++ scripts/dataAnalysis/mergeDataFile.ts
@@ -0,0 +1,36 @@
+import data0 from '../../logData/fractions/logData0';
+import data1 from "../../logData/fractions/logData00";
+import data2 from "../../logData/fractions/logData1";
+import data3 from "../../logData/fractions/logData01";
+import data4 from "../../logData/fractions/logData2";
+import data5 from "../../logData/fractions/logData02";
+
+
+function mergeObjects(...objects : any[]){
+    var keys = getKeysOfObjects(...objects);
+    var obj: any = {};
+    for(let key of keys){
+        for(let object of objects){
+            if(typeof object[key] === "number"){
+                obj[key] = obj[key] ? obj[key] + object[key] : object[key];
+            }else{
+                if(!obj[key]) obj[key] = {};
+                let childKeys = Object.keys(object[key]);
+                for(let childKey of childKeys){
+                    obj[key][childKey] = obj[key][childKey] ? obj[key][childKey] + object[key][childKey] : object[key][childKey];
+                }
+            }
+        }
+    }
+    console.log(obj);
+}
+
+function getKeysOfObjects(...objects : any[]){
+    var keys: any[] = [];
+    for(let object of objects){
+        keys = keys.concat(Object.keys(object));
+    }
+    return Array.from(new Set(keys));
+}
+
+mergeObjects(data0, data1, data2, data3, data4, data5);
